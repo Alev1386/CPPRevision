7e44543de76eb6b076db9d4274f0b2f4620ebeb2
Alex Yevstifeev <evstifeev2005@yandex.ru>
2025-03-23 21:43:21 +0300

    Added fileSystem project

--- FileSystem/FileSystem/FileSystem.cpp
+++ FileSystem/FileSystem/FileSystem.cpp
@@ -8,35 +8,127 @@ using namespace std;
 
 namespace fs = filesystem;
 
+
+void createFile(const string& _folder);
+void writeToFile(const string& _folder);
+vector<fs::path> showFiles(const string& _folder);
+void readFile(const string& _folder);
+void removeFile(const string& _folder);
+
+
 int main()
 {
     string folder = "./files";
-	string filename = "text";
+	string filename;
 
 	if (!fs::exists("files"))
 	{
 		fs::create_directory("files");
 		cout << "Folder	'files' is been created!\n";
 	}
-	for (int i = 0; i < 10; i++)
+	int option = 0;
+	while (option != 6)
 	{
-		ofstream outFile(folder + "/" + filename + to_string(i) + ".txt");
-		outFile << "Hello world!\n";
-		outFile.close();
+		cout << "1.Create file\n2.Write to file\n3.Read file\n4.Remove file\n5.Show files\n6.Exit\n";
+		cin >> option;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		switch (option)
+		{
+		case 1:
+			createFile(folder);
+			break;
+		case 2:
+			writeToFile(folder);
+			break;
+		case 3:
+			readFile(folder);
+			break;
+		case 4:
+			removeFile(folder);
+			break;
+		case 5:
+			showFiles(folder);
+			break;
+		case 6:
+			break;
+		default:
+			break;
+		}
 	}
 
-
-    cout << "Files at: " << folder << ":\n";
-
-	for (const auto& entry : fs::directory_iterator(folder))
-	{
-		cout << entry.path().filename() << endl << "It contains:\n";
-		ifstream inFile(folder + "/" + filename);
-		string content;
-		getline(inFile, content);
-		cout << content << endl;
-		inFile.close();
-
-	}
 	return 0;
+}
+
+void createFile(const string& _folder) {
+	string name;
+	cout << "Enter the name of file" << endl;
+	getline(cin, name);
+	ofstream outFile(_folder + "/" + name);
+	outFile.close();
+}
+void writeToFile(const string& _folder){
+	string name;
+	string data;
+
+	cout << "Enter the name of file" << endl;
+	getline(cin, name);
+
+	cout << "Enter the text" << endl;
+	getline(cin, data);
+
+	string filePath = _folder + "/" + name;
+
+	ofstream outFile(filePath);
+	outFile << data << endl;
+	outFile.close();
+}
+vector<fs::path> showFiles(const string& _folder) {
+	std::vector<fs::path> files;
+	int counter = 1;
+	for (const auto& entry : fs::directory_iterator(_folder))
+	{
+		if (fs::is_regular_file(entry)) {
+			cout << counter << ". " << entry.path().filename() << endl;
+			files.push_back(entry.path());
+			++counter;
+		}
+	}
+	cout << "\n\n\n";
+	return files;
+}
+void readFile(const string& _folder) {
+	vector<fs::path> files = showFiles(_folder);
+	int option = 0;
+	cout << "Enter the index of file! (1-" << files.size()+1 << ")" << endl;
+	while (option < 1 || option > files.size())
+	{
+		cin >> option;
+	}
+
+	fs::path fileName = files[option - 1];
+
+	cout << "Opening file: " << fileName << endl;
+
+	ifstream file(fileName);
+	if (file) {
+		string line;
+		while (getline(file, line)) {
+			cout << line << std::endl;
+		}
+	}
+	else {
+		cerr << "Error opening the file!" << std::endl;
+	}
+}
+
+void removeFile(const string& _folder) {
+	vector<fs::path> files = showFiles(_folder);
+	int option = 0;
+	cout << "Enter the index of file! (1-" << files.size() + 1 << ")" << endl;
+	while (option < 1 || option > files.size())
+	{
+		cin >> option;
+	}
+
+	fs::remove(files[option-1]);
 }
