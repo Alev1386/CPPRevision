88e62370b5157a8ed1e95e52d27574f43118efa2
Alex Yevstifeev <evstifeev2005@yandex.ru>
2025-03-27 22:03:52 +0300

    added complex numbers class

---
+++ ComplexNumbersWithOverload/ComplexNumbersWithOverload/ComplexNumbersWithOverload.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+class Complex
+{
+public:
+	Complex(double r = 0, double i = 0) : real(r), imag(i) {}
+
+	Complex operator-(const Complex& other) {
+		return Complex(real - other.real, imag - other.imag);
+	}
+	Complex operator+(const Complex& other) {
+		return Complex(real + other.real, imag + other.imag);
+	}
+	double operator[](int index) const {
+		if (index == 0) return real;
+		if (index == 1) return imag;
+		throw std::out_of_range("Index must be 0(real) or 1(imag)!");
+	}
+
+	void operator()()const {
+		std::cout << "Complex number: " << real << " + " << imag << "i\n";
+	}
+
+	friend std::ostream& operator <<(std::ostream& out, const Complex& c) {
+		out << c.real << " + " << c.imag << "i";
+		return out;
+	}
+
+private:
+	double real, imag;
+};
+
+
+int main()
+{
+	Complex a(3, 4), b(1, 2);
+
+	Complex sum = a + b;
+	Complex diff = a - b;
+
+	std::cout << "Sum: " << sum << std::endl;
+	std::cout << "Difference: " << diff << std::endl;
+
+	std::cout << "Real part: " << a[0] << std::endl << "Imaginary part: " << a[1] << std::endl;
+
+	a();
+
+	return 0;
+}
---
+++ unordered_map/unordered_map/unordered_map.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+unordered_map<int, long long> cache;
+
+long long factorial(int n) {
+	if (n == 0 || n == 1)
+	{
+		return 1;
+	}
+
+	if (cache.find(n) != cache.end())
+	{
+		return cache[n];
+	}
+
+	return cache[n] = n * factorial(n - 1);
+}
+
+int main()
+{
+	cout << factorial(5) << endl;
+	cout << factorial(10) << endl;
+
+	return 0;
+}
